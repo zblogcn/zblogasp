064392447eb6fc301c78dee9ba3fe8e66dc8480f
hongtianyijian@gmail.com <hongtianyijian@gmail.com>
2012-09-15 16:21:09 +0000

    想了下，还是把分类列表的JS放主题目录比较好

---
+++ zb_users/THEME/default/SCRIPT/custom.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){ 
+	$("ul.ul-subcates").prev("a").before("<span class='sh'>-</span>");
+	$("span.sh").click(function (){
+		$(this).next().next("ul").toggle("fast");
+	})
+	.toggle(
+		function () {
+		$(this).html("+");
+		},
+		function () {
+		$(this).html("-");
+	});
+
+});
