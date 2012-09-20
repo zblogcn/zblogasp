39a5d228db6e0d02b827848b0d4fbb172227eb1e
rainbowsoft@gmail.com <rainbowsoft@gmail.com@5686f369-5255-8811-38ec-a41c3ad2ff07>
2012-09-20 03:31:55 +0000

    APP中心完成度30%;

--- trunk/zb_users/PLUGIN/AppCentre/theme.js
+++ trunk/zb_users/PLUGIN/AppCentre/theme.js
@@ -1,6 +1,6 @@
 $(document).ready(function(){ 
 
-$("#divMain2").prepend("<form class='search' name='edit' id='edit' method='post' enctype='multipart/form-data' action='"+bloghost+"cmd.asp?act=FileUpload'><p>上传主题zti文件:&nbsp;<input type='file' id='edtFileLoad' name='edtFileLoad' size='40' />&nbsp;&nbsp;&nbsp;&nbsp;<input type='submit' class='button' value='提交' name='B1' />&nbsp;&nbsp;<input class='button' type='reset' value='重置' name='B2' />&nbsp;</p></form>");
+$("#divMain2").prepend("<form class='search' name='edit' id='edit' method='post' enctype='multipart/form-data' action='"+bloghost+"cmd.asp?act=FileUpload'><p>上传主题zba文件:&nbsp;<input type='file' id='edtFileLoad' name='edtFileLoad' size='40' />&nbsp;&nbsp;&nbsp;&nbsp;<input type='submit' class='button' value='提交' name='B1' />&nbsp;&nbsp;<input class='button' type='reset' value='重置' name='B2' />&nbsp;</p></form>");
 
 
 $(".theme").each(function(){
@@ -10,7 +10,7 @@ $(".theme").each(function(){
 	s=s+"&nbsp;&nbsp;&nbsp;&nbsp;<a href='"+bloghost+"zb_users/plugin/appcentre/theme_pack.asp?id="+t+"' title='导出该主题' target='_blank'><img height='16' width='16' src='"+bloghost+"zb_users/plugin/appcentre/images/download.png'/></a>";
 	s=s+"&nbsp;&nbsp;&nbsp;&nbsp;<a href='id="+t+"' title='更新该主题'><img height='16' width='16' src='"+bloghost+"zb_users/plugin/appcentre/images/refresh.png'/></a>";
 	if($(this).hasClass("theme-other")){
-		s=s+"&nbsp;&nbsp;&nbsp;&nbsp;<a href='id="+t+"' title='删除该主题'><img height='16' width='16' src='"+bloghost+"zb_users/plugin/appcentre/images/delete.png'/></a>";
+		s=s+"&nbsp;&nbsp;&nbsp;&nbsp;<a href='"+bloghost+"zb_users/plugin/appcentre/theme_del.asp?id="+t+"' title='删除该主题' onclick='return window.confirm(\"单击“确定”继续。单击“取消”停止。\");'><img height='16' width='16' src='"+bloghost+"zb_users/plugin/appcentre/images/delete.png'/></a>";
 	}
 	s=s+"</p>";
 	$(this).append(s);
