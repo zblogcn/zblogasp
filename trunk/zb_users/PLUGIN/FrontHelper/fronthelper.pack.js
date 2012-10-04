e0457f1307ba4582d3f3cff3412f91cb89740651
zsxsoft0@gmail.com <zsxsoft0@gmail.com@5686f369-5255-8811-38ec-a41c3ad2ff07>
2012-10-04 14:44:20 +0000

    FrontHelper(testing)

---
+++ trunk/zb_users/PLUGIN/FrontHelper/fronthelper.pack.js
@@ -0,0 +1,37 @@
+$(document).ready(function() {
+    if (GetCookie("username") && GetCookie("password")) {
+        var a,
+        c,
+        b,
+        d = 0;
+        if ($('p[@codetype=post]').length > 0) {
+            d = $('p[@codetype=post]').attr('entryid')
+        }
+        $('p.cloudreamHelperLink').css('display', 'block').css('text-align', 'right').each(function() {
+            a = $(this);
+            c = a.attr('codetype');
+            b = a.attr('entryid');
+            if (c == "comment") {
+                a.append('[<a class="helperLink" href="' + bloghost + 'zb_system/ADMIN/admin_default.asp?act=CommentEdt&amp;id=' + b + '&amp;log_id=' + d + '">编辑</a>]&nbsp;[<a class="helperGet" href="' + bloghost + 'zb_system/cmd.asp?act=CommentDel&amp;id=' + b + '&amp;log_id=' + d + '">删除</a>]')
+            } else if (c == "postmulti" || c == "post") {
+                a.append('[<a class="helperLink" href="' + bloghost + 'zb_system/ADMIN/admin_default.asp?act=ArticleEdt&amp;type=ueditor&amp;id=' + b + '">编辑</a>]')
+            }
+        });
+        $('a.helperLink').attr("target", "_0");
+        $('a.helperGet').click(function(e) {
+            if (window.confirm('确认删除？')) {
+                $this = $(this);
+                $href = $this.attr("href");
+                $this = $this.parent().html("[删除中...]");
+                $.ajax({
+                    url: $href,
+                    dataType: "text",
+                    complete: function() {
+                        $this.html("[已删除]").parent().css("opacity", "0.5")
+                    }
+                })
+            }
+            e.preventDefault()
+        })
+    }
+});
