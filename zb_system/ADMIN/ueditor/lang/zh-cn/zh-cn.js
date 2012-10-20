02529d9a24be9c96032b73ef0b4dfce6083eb230
zsxsoft0@gmail.com <zsxsoft@outlook.com>
2012-10-20 12:55:46 +0000

    修个问题

--- zb_system/ADMIN/ueditor/lang/zh-cn/zh-cn.js
+++ zb_system/ADMIN/ueditor/lang/zh-cn/zh-cn.js
@@ -11,7 +11,7 @@ UE.I18N['zh-cn'] = {
         'italic':'斜体', 'underline':'下划线', 'strikethrough':'删除线', 'subscript':'下标',
         'superscript':'上标', 'formatmatch':'格式刷', 'source':'源代码', 'blockquote':'引用',
         'pasteplain':'纯文本粘贴模式', 'selectall':'全选', 'print':'打印', 'preview':'预览',
-        'horizontal':'分隔线', 'removeformat':'清除格式', 'time':'时间', 'date':'日期',
+        'horizontal':'分隔线(第一次使用时分隔摘要)', 'removeformat':'清除格式', 'time':'时间', 'date':'日期',
         'unlink':'取消链接', 'insertrow':'前插入行', 'insertcol':'前插入列', 'mergeright':'右合并单元格', 'mergedown':'下合并单元格',
         'deleterow':'删除行', 'deletecol':'删除列', 'splittorows':'拆分成行', 'splittocols':'拆分成列', 'splittocells':'完全拆分单元格',
         'mergecells':'合并多个单元格', 'deletetable':'删除表格', 'insertparagraphbeforetable':'表格前插行', 'cleardoc':'清空文档',
