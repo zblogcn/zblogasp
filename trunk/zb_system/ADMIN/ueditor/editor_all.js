c40d0a23c20326ec965319a78623b2e17b5c631c
zsxsoft0 <zsxsoft0@5686f369-5255-8811-38ec-a41c3ad2ff07>
2013-01-01 06:53:57 +0000

    修正uEditor链接出错

--- trunk/zb_system/ADMIN/ueditor/editor_all.js
+++ trunk/zb_system/ADMIN/ueditor/editor_all.js
@@ -9798,8 +9798,8 @@ UE.plugins['fiximgclick'] = function() {
                     a.href = a.innerHTML = a.innerHTML.replace(/<[^>]+>/g,'');
                     href = a.getAttribute("href").replace(new RegExp(domUtils.fillChar,'g'),'');
                     href = /^(?:https?:\/\/)/ig.test(href) ? href : "http://"+ href;
-                    a.setAttribute('data_ue_src',href);
-                    a.href = href;
+                    a.setAttribute('data_ue_src',utils.html(href));
+                    a.href = utils.html(href);
 
                     range.insertNode(a);
                     a.parentNode.insertBefore(text, a.nextSibling);
