aa3984f0427ae8858d61958083e969e00e47918d
rainbowsoft <rainbowsoft@users.noreply.github.com>
2013-01-08 21:25:53 +0000

    zblogmakefileindex

---
+++ zblogmakefileindex/zblogmakefileindex/Form1.Designer.vb
@@ -0,0 +1,96 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 重写 Dispose，以清理组件列表。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Windows 窗体设计器所必需的
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下过程是 Windows 窗体设计器所必需的
+    '可以使用 Windows 窗体设计器修改它。
+    '不要使用代码编辑器修改它。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.TextBox1 = New System.Windows.Forms.TextBox
+        Me.TextBox2 = New System.Windows.Forms.TextBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Enabled = False
+        Me.TextBox1.Location = New System.Drawing.Point(12, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(612, 21)
+        Me.TextBox1.TabIndex = 0
+        '
+        'TextBox2
+        '
+        Me.TextBox2.Location = New System.Drawing.Point(12, 54)
+        Me.TextBox2.Multiline = True
+        Me.TextBox2.Name = "TextBox2"
+        Me.TextBox2.Size = New System.Drawing.Size(612, 342)
+        Me.TextBox2.TabIndex = 1
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 425)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(118, 23)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "生成XML索引"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(508, 425)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(116, 23)
+        Me.Button2.TabIndex = 3
+        Me.Button2.Text = "生成容差XML"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(356, 425)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(121, 20)
+        Me.ComboBox1.TabIndex = 4
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(636, 460)
+        Me.Controls.Add(Me.ComboBox1)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.TextBox2)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Name = "Form1"
+        Me.Text = "Z-Blog File Index Maker"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents TextBox2 As System.Windows.Forms.TextBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+
+End Class
---
+++ zblogmakefileindex/zblogmakefileindex/Form1.vb
@@ -0,0 +1,308 @@
+Public Class Form1
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        'TextBox2.Text = My.Computer.FileSystem.GetFiles(TextBox1.Text, FileIO.SearchOption.SearchAllSubDirectories).Count
+
+
+
+
+        For Each c As String In My.Computer.FileSystem.GetDirectories(My.Application.Info.DirectoryPath, FileIO.SearchOption.SearchTopLevelOnly)
+
+            Dim b As String = ""
+            Dim d As String = ""
+            Dim f As String = ""
+            b += ("<files build=''>" + vbCrLf)
+            For Each a As String In My.Computer.FileSystem.GetFiles(c, FileIO.SearchOption.SearchAllSubDirectories)
+                b += vbTab + "<file"
+
+                b += " name='" + a.Replace(c + "\", "") + "'"
+
+                Dim bytMD5 As Byte() = New System.Security.Cryptography.MD5CryptoServiceProvider().ComputeHash(My.Computer.FileSystem.ReadAllBytes(a))
+
+                d = BitConverter.ToString(bytMD5).Replace("-", String.Empty).ToUpper
+
+                'b += " md5='" + d + "'"
+
+
+                Dim g As New Crc32
+                f = Convert.ToString(g.CalculateBlock(My.Computer.FileSystem.ReadAllBytes(a)), 16).ToUpper
+
+                b += " crc32='" + f + "'"
+
+                b += "/>" + vbCrLf
+            Next
+            b += "</files>"
+            My.Computer.FileSystem.WriteAllText(My.Computer.FileSystem.GetDirectoryInfo(c).Name + ".xml", b, False)
+            Me.TextBox2.AppendText("生成文件:" + My.Computer.FileSystem.GetDirectoryInfo(c).Name + ".xml" + vbCrLf)
+        Next
+
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+
+
+        Dim a As Integer = Me.ComboBox1.SelectedIndex + 1
+        Dim b As String = Me.ComboBox1.Items(a - 1)
+        Dim f(a) As Collections.Generic.Dictionary(Of String, String)
+        Dim f2(a) As Collections.Generic.Dictionary(Of String, String)
+
+        For i As Integer = 1 To a
+            f2(i) = New Collections.Generic.Dictionary(Of String, String)
+        Next
+
+        Dim x As New Xml.XmlDocument
+        For i As Integer = 1 To a
+            f(i) = New Collections.Generic.Dictionary(Of String, String)
+            x.Load(My.Application.Info.DirectoryPath + "\" + Me.ComboBox1.Items(i - 1) + ".xml")
+            For Each y As Xml.XmlNode In x.DocumentElement.SelectNodes("file")
+                If y.Attributes("name").InnerText.Contains("zb_users\") = False Then
+
+                    f(i).Add(y.Attributes("name").InnerText, y.Attributes("crc32").InnerText)
+                Else
+                    If y.Attributes("name").InnerText.Contains("zb_users\LANGUAGE\SimpChinese.asp") = True Then
+                        f(i).Add(y.Attributes("name").InnerText, y.Attributes("crc32").InnerText)
+                    End If
+
+                    If y.Attributes("name").InnerText.Contains("zb_users\EMOTION\") = True Then
+                        f(i).Add(y.Attributes("name").InnerText, y.Attributes("crc32").InnerText)
+                    End If
+
+                End If
+
+            Next
+            'MsgBox(f(i).Count)
+        Next
+
+        For i As Integer = a To 2 Step -1
+            If i = a Then
+                For Each s As String In f(i).Keys
+
+                    If f(i - 1).ContainsKey(s) = False Then
+                        Me.TextBox2.AppendText(s + vbCrLf)
+                        f2(i).Add(s, f(i)(s))
+                    Else
+                        If String.Compare(f(i - 1)(s), f(i)(s)) <> 0 Then
+                            Me.TextBox2.AppendText(s + vbCrLf)
+                            f2(i).Add(s, f(i)(s))
+                        End If
+
+                    End If
+
+                Next
+            Else
+                'For Each s As String In f2(i + 1).Keys
+                '    If f(i - 1).ContainsKey(s) = False Then
+                '        Me.TextBox2.AppendText(s + vbCrLf)
+                '        f2(i).Add(s, f2(i + 1)(s))
+                '    Else
+                '        If String.Compare(f(i - 1)(s), f2(i + 1)(s)) <> 0 Then
+                '            Me.TextBox2.AppendText(s + vbCrLf)
+                '            f2(i).Add(s, f2(i + 1)(s))
+                '        End If
+                '    End If
+                'Next
+                For Each s As String In f(i).Keys
+                    If f(i - 1).ContainsKey(s) = False Then
+                        Me.TextBox2.AppendText(s + vbCrLf)
+                        f2(i).Add(s, f(i)(s))
+                    Else
+                        If String.Compare(f(i - 1)(s), f(i)(s)) <> 0 Then
+                            Me.TextBox2.AppendText(s + vbCrLf)
+                            f2(i).Add(s, f(i)(s))
+                        End If
+                    End If
+                Next
+                For Each s As String In f2(i + 1).Keys
+                    If f2(i).ContainsKey(s) = False Then
+                        f2(i).Add(s, f2(i + 1)(s))
+                    End If
+                Next
+
+            End If
+            'MsgBox(f2(i).Count)
+        Next
+
+        For i As Integer = 0 To a - 2
+
+            Dim s As String = Nothing
+            Dim d As String = Nothing
+
+            s = My.Application.Info.DirectoryPath + "\" + Me.ComboBox1.Items(i) + "-" + Me.ComboBox1.Items(a - 1) + ".xml"
+            Dim t As String = Nothing
+
+
+
+            t = "<files codepage='65001' xmlns:dt='urn:schemas-microsoft-com:datatypes'>" + vbCrLf
+            For Each j As String In f2(i + 2).Keys
+                If My.Computer.FileSystem.FileExists(My.Application.Info.DirectoryPath + "\" + b + "\" + j) = True Then
+
+                    d = System.Convert.ToBase64String(My.Computer.FileSystem.ReadAllBytes(My.Application.Info.DirectoryPath + "\" + b + "\" + j))
+
+                    Dim g As New Crc32
+                    Dim m As String = Convert.ToString(g.CalculateBlock(My.Computer.FileSystem.ReadAllBytes(My.Application.Info.DirectoryPath + "\" + b + "\" + j)), 16).ToUpper
+
+                    t += vbTab + "<file name='" + j + "' crc32='" + m + "'  dt:dt='bin.base64'>" + d + "</file>" + vbCrLf
+                End If
+            Next
+            t += "</files>"
+
+            My.Computer.FileSystem.WriteAllText(s, t, False)
+
+
+        Next
+
+
+    End Sub
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+
+        Dim b As String = ""
+        Dim i As Integer = 0
+        b += "<builds>" + vbCrLf
+        For Each c As String In My.Computer.FileSystem.GetDirectories(My.Application.Info.DirectoryPath, FileIO.SearchOption.SearchTopLevelOnly)
+            b += vbTab + "<build>" + My.Computer.FileSystem.GetDirectoryInfo(c).Name + "</build>" + vbCrLf
+            i += 1
+        Next
+        b += "</builds>"
+        My.Computer.FileSystem.WriteAllText(My.Application.Info.DirectoryPath + "\builds.xml", b, False)
+        Me.TextBox1.Text = "发现了" + i.ToString+"个版本"
+
+
+
+        Dim x As New Xml.XmlDocument
+        'MsgBox(My.Application.Info.DirectoryPath)
+        x.Load(My.Application.Info.DirectoryPath + "\builds.xml")
+
+        For Each y As Xml.XmlNode In x.DocumentElement.SelectNodes("build")
+            Me.ComboBox1.Items.Add(y.InnerText)
+            Me.ComboBox1.Text = y.InnerText
+        Next
+
+
+
+    End Sub
+End Class
+
+
+
+Public Class Crc32
+    Private Const TABLESIZE As Integer = 256
+    Private Const DEFAULTPOLYNOMIAL As Integer = &HEDB88320
+    Private Const DEFAULTINITIALVALUE As Integer = &HFFFFFFFF
+    Private lookup(TABLESIZE - 1) As Integer
+    Private crcPolynomial As Integer = 0
+    Public Sub New()
+        Me.New(DEFAULTPOLYNOMIAL)
+    End Sub
+    Public Sub New(ByVal crcPolynomial As Integer)
+        Me.crcPolynomial = crcPolynomial
+        InitLookupTable()
+    End Sub
+    Public Property Polynomial() As Integer
+        Get
+            Return crcPolynomial
+        End Get
+        Set(ByVal Value As Integer)
+            Me.crcPolynomial = value
+            InitLookupTable()
+        End Set
+    End Property
+    Public Overloads Function CalculateBlock(ByVal bytes() As Byte) _
+                                             As Integer
+        Return CalculateBlock(bytes, 0, bytes.Length)
+    End Function
+    Public Overloads Function CalculateBlock(ByVal bytes() As Byte, _
+                                             ByVal index As Integer, _
+                                             ByVal length As Integer _
+                                            ) As Integer
+        Return CalculateBlock(bytes, index, length, DEFAULTINITIALVALUE)
+    End Function
+    Public Overloads Function CalculateBlock( _
+                              ByVal bytes() As Byte, _
+                              ByVal index As Integer, _
+                              ByVal length As Integer, _
+                              ByVal initialValue As Integer) _
+                              As Integer
+        If bytes Is Nothing Then
+            Throw New ArgumentNullException("CalculateBlock(): bytes")
+        ElseIf index < 0 Or length <= 0 _
+               Or index + length > bytes.Length Then
+            Throw New ArgumentOutOfRangeException()
+        End If
+        Return Not InternalCalculateBlock(bytes, index, _
+                                          length, initialValue)
+    End Function
+    Private Function InternalCalculateBlock( _
+                     ByVal bytes() As Byte, _
+                     ByVal index As Integer, _
+                     ByVal length As Integer, _
+                     ByVal initialValue As Integer) _
+                     As Integer
+        Dim crc As Integer = initialValue
+        Dim shiftedCrc As Integer
+        Dim position As Integer
+        For position = index To length - 1
+            shiftedCrc = crc And &HFFFFFF00
+            shiftedCrc = shiftedCrc \ &H100
+            shiftedCrc = shiftedCrc And &HFFFFFF
+            crc = shiftedCrc Xor lookup(bytes(position) Xor _
+                                                        (crc And &HFF))
+        Next
+        Return crc
+    End Function
+    Public Overloads Function CalculateFile(ByVal path As String) _
+                                            As Integer
+        Return CalculateFile(path, DEFAULTINITIALVALUE)
+    End Function
+    Public Overloads Function CalculateFile( _
+                              ByVal path As String, _
+                              ByVal initialValue As Integer) _
+                              As Integer
+        If path Is Nothing Then
+            Throw New ArgumentNullException("path")
+        ElseIf path.Length = 0 Then
+            Throw New ArgumentException("Invalid path")
+        End If
+        Return Not InternalCalculateFile(path, initialValue)
+    End Function
+    Private Function InternalCalculateFile( _
+                     ByVal path As String, _
+                     ByVal initialValue As Integer) _
+                     As Integer
+        Const blockSize As Integer = 4096
+        Dim count As Integer
+        Dim inStream As IO.FileStream
+        Dim bytes(blockSize - 1) As Byte
+        Dim crc As Integer = initialValue
+        Try
+            inStream = IO.File.Open(path, IO.FileMode.Open, IO.FileAccess.Read)
+            While inStream.Position < inStream.Length
+                count = inStream.Read(bytes, 0, blockSize)
+                crc = InternalCalculateBlock(bytes, 0, count, crc)
+            End While
+        Finally
+            If Not inStream Is Nothing Then
+                inStream.Close()
+            End If
+        End Try
+        Return crc
+    End Function
+    Private Sub InitLookupTable()
+        Dim byteCount, bitCount As Integer
+        Dim crc, shiftedCrc As Integer
+        For byteCount = 0 To TABLESIZE - 1
+            crc = byteCount
+            For bitCount = 0 To 7
+                shiftedCrc = crc And &HFFFFFFFE
+                shiftedCrc = shiftedCrc \ &H2
+                shiftedCrc = shiftedCrc And &H7FFFFFFF
+                If (crc And &H1) Then
+                    crc = shiftedCrc Xor crcPolynomial
+                Else
+                    crc = shiftedCrc
+                End If
+            Next
+            lookup(byteCount) = crc
+        Next
+    End Sub
+End Class
