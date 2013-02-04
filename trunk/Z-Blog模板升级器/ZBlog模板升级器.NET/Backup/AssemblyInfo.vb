d65ea87510f3d3a7c27346292e53da899b33aea0
zsxsoft0 <zsxsoft0@5686f369-5255-8811-38ec-a41c3ad2ff07>
2013-02-04 07:24:12 +0000

    update to vs2012

---
+++ trunk/Z-Blog模板升级器/ZBlog模板升级器.NET/Backup/AssemblyInfo.vb
@@ -0,0 +1,33 @@
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
+Imports System.Runtime.InteropServices
+
+' 有关程序集的常规信息是通过下列
+' 属性集控制的。更改这些属性值可
+' 修改与程序集关联的信息。 
+
+
+' TODO: 检查程序集的属性值
+
+
+<Assembly: AssemblyTitle("")>
+<Assembly: AssemblyDescription("")>
+<Assembly: AssemblyCompany("")>
+<Assembly: AssemblyProduct("")>
+<Assembly: AssemblyCopyright("")>
+<Assembly: AssemblyTrademark("")>
+<Assembly: AssemblyCulture("")>
+
+' 程序集的版本信息由下列 4 个值组成:
+
+'	主版本
+'	次版本
+'	内部版本号
+'	修订
+
+' 您可以指定所有这些值，也可以使用“内部版本号”和“修订号”的默认值，
+' 方法是按如下所示使用“*”:
+
+<Assembly:  AssemblyVersion("1.0.*")>
+
+
