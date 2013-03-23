6051bc0162e147395a4ff9d990a25890d3347c2f
rainbowsoft@gmail.com <rainbowsoft@gmail.com@5686f369-5255-8811-38ec-a41c3ad2ff07>
2013-03-23 05:28:41 +0000

    把update的index.asp提上去了

---
+++ trunk/zblogupdate/zblogupdate/index.aspx.vb
@@ -0,0 +1,58 @@
+Partial Public Class _Default
+    Inherits System.Web.UI.Page
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+
+        Response.Clear()
+        If Request.RawUrl.Contains("?install") = True Then
+            Dim s As String = ""
+            If Application("Release") = Nothing Then
+                s = My.Computer.FileSystem.ReadAllText(System.Web.HttpContext.Current.Request.PhysicalApplicationPath & "zblog2\" & "Release.xml")
+                Application("Release") = s
+            Else
+                s = Application("Release")
+            End If
+            System.Web.HttpContext.Current.Response.Filter = New IO.Compression.GZipStream(System.Web.HttpContext.Current.Response.Filter, IO.Compression.CompressionMode.Compress)
+            System.Web.HttpContext.Current.Response.AppendHeader("Content-Encoding", "gzip")
+            System.Web.HttpContext.Current.Response.ContentType = "text/xml"
+            System.Web.HttpContext.Current.Response.AppendHeader("Last-Modified", System.DateTime.Parse(My.Computer.FileSystem.GetFileInfo(System.Web.HttpContext.Current.Request.PhysicalApplicationPath & "zblog2\" & "Release.xml").LastWriteTime).ToUniversalTime.ToString("r", System.Globalization.DateTimeFormatInfo.InvariantInfo))
+            Response.Write(s)
+        ElseIf Request.RawUrl.Contains("?beta") = True Then
+            Dim s As String = System.Web.HttpContext.Current.Request.PhysicalApplicationPath & "zblog2\index.html"
+            Response.ContentEncoding = System.Text.Encoding.UTF8
+            Response.ContentType = "text/plain"
+            Response.WriteFile(s)
+        ElseIf Request.QueryString.Count > 0 Then
+
+            If System.Web.HttpContext.Current.Request.Headers("Accept-Encoding") IsNot Nothing Then
+                If System.Web.HttpContext.Current.Request.Headers("Accept-Encoding").Contains("gzip") = True Then
+                    System.Web.HttpContext.Current.Response.Filter = New IO.Compression.GZipStream(System.Web.HttpContext.Current.Response.Filter, IO.Compression.CompressionMode.Compress)
+                    System.Web.HttpContext.Current.Response.AppendHeader("Content-Encoding", "gzip")
+                ElseIf System.Web.HttpContext.Current.Request.Headers("Accept-Encoding").Contains("deflate") = True Then
+                    System.Web.HttpContext.Current.Response.Filter = New IO.Compression.DeflateStream(System.Web.HttpContext.Current.Response.Filter, IO.Compression.CompressionMode.Compress)
+                    System.Web.HttpContext.Current.Response.AppendHeader("Content-Encoding", "deflate")
+                End If
+            End If
+
+            System.Web.HttpContext.Current.Response.ContentType = "application/octet-stream"
+            Dim s As String = System.Web.HttpContext.Current.Request.PhysicalApplicationPath & "zblog2\" & Request.QueryString.Get(0)
+            If s.Contains("../") = False And s.Contains("..\") = False Then
+                Try
+                    Response.WriteFile(s)
+                Catch ex As Exception
+
+                End Try
+
+            End If
+
+        Else
+            Dim s As String = System.Web.HttpContext.Current.Request.PhysicalApplicationPath & "zblog2\index.html"
+            Response.ContentEncoding = System.Text.Encoding.UTF8
+            Response.ContentType = "text/plain"
+            Response.WriteFile(s)
+        End If
+
+        Response.End()
+    End Sub
+
+End Class
